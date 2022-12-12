f621d84f5dc06821d25fc43b8471b6e2a60099ac
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-12-12 11:08:36 +0100

    Nombre y Apellido Estudiante 1

--- src/pruebas/Prueba.java
+++ src/pruebas/Prueba.java
@@ -2,8 +2,8 @@ package pruebas;
 
 public class Prueba {
     public static void main(String[] args) {
-        String nombre = "";
-        String apellido = "";
+        String nombre = "Antonio";
+        String apellido = "Navarro";
 
         System.out.println("Nombre: " + nombre + " Apellidos: " + apellido);
     } // € Hecho por Antonio Navarro y Fernando Miguel
