218c6dafeff57b423c3eb000178d95e538d32650
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-12-12 10:45:23 +0100

    first commit

---
+++ src/pruebas/Prueba.java
@@ -0,0 +1,10 @@
+package pruebas;
+
+public class Prueba {
+    public static void main(String[] args) {
+        String nombre = "";
+        String apellido = "";
+
+        System.out.println("Nombre: " + nombre + " Apellidos: " + apellido);
+    } // € Hecho por Antonio Navarro y Fernando Miguel
+}
